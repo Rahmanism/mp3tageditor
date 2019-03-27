159632e28e978eda8be3af804aff7b06c33276c5
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-03-27 10:37:56 +0430

    mp3panel is designed! and loaded in the main file.

--- mp3tag.py
+++ mp3tag.py
@@ -3,6 +3,8 @@ from mp3panel import Mp3Panel
 from myframe import MyFrame
 
 if __name__ == '__main__':
-    app = wx.App()
+    app = wx.App(False)
     frame = MyFrame()
+    # mp3panel = Mp3Panel(frame)
+    # mp3panel.SetSize(wx.Window.GetClientSize(frame))
     app.MainLoop()
--- myframe.py
+++ myframe.py
@@ -1,7 +1,9 @@
 import wx
+from mp3panel import Mp3Panel
 
 
 class MyFrame(wx.Frame):
     def __init__(self):
         super().__init__(parent=None, title='MP3 Tag Editor')
+        self.panel = Mp3Panel(self)
         self.Show()
